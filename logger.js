06d1614b0c2bc47fed843c0af8c25ed72f04f6e2
Loick Hommet <hommet.loick@gmail.com>
2022-04-29 14:24:00 +0200

    ok : ajoue de la condition si le nombre n'est ni un multiple de 5 ni un multiple de 3
    Resulta:
    Nombre envoyé
    4 n'est ni un multiple de 5 ni un multiple de 3
    nombre Recu:  4

--- logger.js
+++ logger.js
@@ -11,6 +11,9 @@ logger.on('nombreRecu', function(number){
     if((number % 5 == 0) & (number % 3 == 0)){
         console.log("FizzBuzz");
     }
+    if((number % 5 != 0) & (number % 3 != 0)){
+        console.log(number + ' n\'est ni un multiple de 5 ni un multiple de 3');
+    }
     console.log("nombre Recu: ", number)
 });
 
